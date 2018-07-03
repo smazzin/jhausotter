c990d8a364b31cd920c825e5b2bf14e0d509db4b
Josh Hausotter <jhausotter@gmail.com>
2018-07-02 21:58:04 -0700

    Updated nav menu for mobile and desktop

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,21 @@
-(function () {
-  
-  var navButton = document.getElementById("nav-menu-button"),
-      navUl = document.getElementsByClassName("nav-ul");
-     
-  function toggleMobileMenu() {
-    navUl[0].style.transition = "max-height 0.5s";
-    navUl[0].classList.toggle("hide-ul");
-  }
-  
-  navButton.onclick = toggleMobileMenu;
-}());
+// (function () {
+
+//   var navButton = document.getElementById("nav-menu-button"),
+//       navUl = document.getElementsByClassName("nav-ul");
+//       navHeader = document.getElementsByClassName("bg-none");
+
+//   function toggleMobileMenu() {
+//     navUl[0].style.transition = "max-height 0.5s";
+//     navUl[0].classList.toggle("hide-ul");
+//     navHeader[0].classList.toggle("bg-white");
+//   }
+
+//   navButton.onclick = toggleMobileMenu;
+// }());
+
+// $('.menu-toggle').click(function() {
+
+//   $('.site-nav').toggleClass('site-nav--open', 500);
+//   $(this).toggleClass('open');
+
+// })
